eb5a6d42230a9e95ffad36c34816bc690d7b5174
Lucas Gomes <lucas.g.oliveira1@hotmail.com>
2024-10-14 22:10:24 -0300

    feat: generate token to authenticate

---
+++ oauth2/src/main/java/br/com/lucas/oauth2/services/TokenService.java
@@ -0,0 +1,36 @@
+package br.com.lucas.oauth2.services;
+
+import br.com.lucas.oauth2.models.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class TokenService {
+
+    @Value("${jwt.private.key}")
+    private String jwtKey;
+
+    public String generateToken(User user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(jwtKey);
+
+            return JWT.create()
+                    .withIssuer("oauth2")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error while authenticating");
+        }
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
